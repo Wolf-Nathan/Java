4e2101981d12f557fc3f465fbc0829ab2dfc71f9
Wolf Nathan <nathan-w@hotmail.fr>
2019-12-10 16:13:11 +0100

    [POKEDECK] When a card is add an ID is automatically generate

--- Pokedeck/src/AddCard.java
+++ Pokedeck/src/AddCard.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class AddCard extends Menu {
 
@@ -32,7 +33,26 @@ public class AddCard extends Menu {
 			String name = nameText.getText();
 			String description = descriptionText.getText();
 			//int id = Integer.parseInt(idText.getText());
-			Card c = new Card(name, description);
+			int id = 0;
+			boolean test = false;
+			boolean test2 = true;
+			//We generate a new id and verify is not already use
+			do {
+				id = 1000 + (int)(Math.random() * (100000 - 10000));
+				int i = 0;
+				while(i<pokedeck.size() && test2) {
+					Card c = pokedeck.get(i);
+					if(c.getID() == id) {
+						test2 = false;
+					}
+					i++;
+				}
+				if(test2) {
+					test = true;
+				}
+			}while(!test);
+			
+			Card c = new Card(name, description, id);
 			System.out.println(c);
 			pokedeck.add(c);
 			System.out.println(pokedeck);
